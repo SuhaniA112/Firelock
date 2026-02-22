e94e7519763d666f96833588007fea01135cf27e
Andrew Zhang <azhappy888@gmail.com>
2026-02-22 01:05:38 -0500

    a

--- App2.tsx
+++ App2.tsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3001");
 
 function App() {
-  const [arduinoData, setArduinoData] = useState("");
+  const [arduinoData, setArduinoData] = useState("10");
 
-//   useEffect(() => {
-//     socket.on("arduino-data", (data) => {
-//       setArduinoData(data);
-//     });
+  //   useEffect(() => {
+  //     socket.on("arduino-data", (data) => {
+  //       setArduinoData(data);
+  //     });
 
-//     return () => {
-//       socket.off("arduino-data");
-//     };
-//   }, []);
+  //     return () => {
+  //       socket.off("arduino-data");
+  //     };
+  //   }, []);
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Arduino Data:</h1>
-      <p>hi </p> 
-      {/* <p>{arduinoData}</p> */}
-    </div>
+    <View>
+      <Text> {arduinoData}</Text>
+    </View>
   );
 }
 
